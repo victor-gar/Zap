6cf17cfd4df7b0b38828725ba4de9b3381812789
Garitskiy Victor <dinomc4711@gmail.com>
2023-05-07 19:59:23 +0600

    Initial Commit

---
+++ ZapFitness/ZapFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZapFitnessApp.swift
+//  ZapFitness
+//
+//  Created by Victor Garitskyu on 07.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZapFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
